f1c272e90b31d893c5e0b5fcbb69f7d552830c7c
Adrian Sampson <adrian@radbox.org>
2013-08-22 15:17:44 -0700

    use chokidar library

--- vegalive.js
+++ vegalive.js
@@ -2,6 +2,7 @@ var express = require('express');
 var http = require('http');
 var socketio = require('socket.io');
 var fs = require('fs');
+var chokidar = require('chokidar');
 
 var app = express();
 var server = http.createServer(app);
@@ -24,8 +25,18 @@ io.sockets.on('connection', function (socket) {
         fs.exists(filename, function (exists) {
             if (exists) {
                 socket.emit('ready', { 'message': 'watching file' });
-                fs.watch(filename, function (e, f) {
+                // N.B.: Using the polling backend to chokidar here since the
+                // non-polling (fs.watch) backend seems unstable. On my
+                // machine, it was "losing track" of the file after a few
+                // updates. (That's true of using fs.watch directly, too.)
+                var watcher = chokidar.watch(filename);
+                watcher.on('change', function (path) {
+                    console.log('watcher change ' + path);
                     socket.emit('change', { filename: filename });
+                });
+                watcher.on('error', function (error) {
+                    console.log('watcher error: ' + error);
+                    socket.emit('error', { 'message': 'watcher error' });
                 });
             } else {
                 socket.emit('error', { 'message': 'no such file' });
